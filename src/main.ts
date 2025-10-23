36d02c2f1ef270e5312f4066c4e3b2448dafc94f
EneaSema <sema.enea1993@gmail.com>
2025-10-23 12:37:56 +0200

    resolve milestone 1 bonus  guardando il video

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
+// 🏆 Snack 1
+// Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia…
+// Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
+
+// Se è una stringa: stampala in maiuscolo
+// Se è un numero: moltiplicalo per due e stampalo
+// Se è un booleano: stampa “Sì” o “No” in base al suo valore
+// In tutti gli altri casi: stampa “Tipo non supportato”
+
 let valore: unknown;
 
 if (typeof valore === "string") {
@@ -9,6 +18,17 @@ if (typeof valore === "string") {
   if ((valore = true)) {
     console.log(valore ? "SI" : "NO");
   }
+}
+// 🎯 BONUS
+// Se è null: stampa “Il dato è vuoto”
+// Se è un array: stampa la sua lunghezza
+// Se è una Promise: attendi che si risolva e stampa il valore del resolve.
+else if (valore === null) {
+  console.log("Il dato è vuoto");
+} else if (Array.isArray(valore)) {
+  console.log(valore.length);
+} else if (valore instanceof Promise) {
+  valore.then((msg) => console.log(msg));
 } else {
   console.log("Valore non supportato");
 }
